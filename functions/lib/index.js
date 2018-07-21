c9edb4e103acc44c2ae08987560dee06d7ae5aff
krypt0r <imhsingh16@gmail.com>
2018-07-21 11:24:13 +0530

    Functions

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -14,26 +14,31 @@ app.intent("What is the power output", conv => {
     console.log("Appliances", appliances);
     console.log("num", numberOfAppliances);
     //Below code for database ref
-    return database.ref("appliance/").once("value", snapshot => {
+    return database.ref("appliances/").once("value", snapshot => {
         const data = snapshot.val();
         //now data is a js object, the appliances detail could be accessed similarily
-        let totalPowerConsumption = 1;
+        let totalPowerConsumption = 0;
         if (appliances.length === numberOfAppliances.length) {
             for (let i = 0; i < appliances.length; i++) {
-                totalPowerConsumption += numberOfAppliances[i] * data[appliances[i].output];
+                totalPowerConsumption += numberOfAppliances[i] * data[appliances[i]].output;
             }
         }
         else if (numberOfAppliances.length === 1) {
             for (let i of appliances) {
-                totalPowerConsumption += numberOfAppliances[0] * data[i.output];
+                totalPowerConsumption += numberOfAppliances[0] * data[i].output;
+            }
+        }
+        else if (numberOfAppliances.length === 0) {
+            for (let i of appliances) {
+                totalPowerConsumption += data[appliances[i]].output;
             }
         }
         else {
             for (let i = 0; i < numberOfAppliances.length; i++) {
-                totalPowerConsumption += numberOfAppliances[i] * data[appliances[i].output];
+                totalPowerConsumption += numberOfAppliances[i] * data[appliances[i]].output;
             }
-            for (let i = numberOfAppliances.lenght; i < appliances.length; i++) {
-                totalPowerConsumption += data[appliances[i].output];
+            for (let i = numberOfAppliances.length; i < appliances.length; i++) {
+                totalPowerConsumption += data[appliances[i]].output;
             }
         }
         conv.ask("Total Power Consumption is " + totalPowerConsumption);
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -11,36 +11,37 @@ const app = dialogflow({
 
 
 app.intent("What is the power output", conv=> {
-    const appliances = conv.parameters['appliances'];
-    const numberOfAppliances = conv.parameters['numbers'];
-    console.log("Appliances", appliances)
-    console.log("num", numberOfAppliances)
+    const appliances: string[] = conv.parameters['appliances'];
+    const numberOfAppliances: number[] = conv.parameters['numbers'];
     //Below code for database ref
-    return database.ref("appliance/").once("value",snapshot=>{
+    return database.ref("appliances/").once("value",snapshot=>{
             const data = snapshot.val();
             //now data is a js object, the appliances detail could be accessed similarily
-            let totalPowerConsumption = 1;
+            let totalPowerConsumption = 0;
             if(appliances.length === numberOfAppliances.length){
                 for(let i = 0; i<appliances.length; i++){
-                    totalPowerConsumption += numberOfAppliances[i]*data[appliances[i].output];
+                    totalPowerConsumption += numberOfAppliances[i]*data[appliances[i]].output;
                 }
             }
             else if(numberOfAppliances.length === 1){
                      for(let i of appliances){
-                    totalPowerConsumption += numberOfAppliances[0]*data[i.output];
+                    totalPowerConsumption += numberOfAppliances[0]*data[i].output;
                     }
                  }
-                 else{
-                    for(let i = 0; i < numberOfAppliances.length; i++){
-                        totalPowerConsumption += numberOfAppliances[i]*data[appliances[i].output];
+                 else if(numberOfAppliances.length === null){
+                        for(let i of appliances){
+                            totalPowerConsumption += data[appliances[i]].output;
+                        }
                     }
-                    for(let i = numberOfAppliances.lenght; i<appliances.length; i++){
-                        totalPowerConsumption += data[appliances[i].output];
+                    else{
+                        for(let i = 0; i < numberOfAppliances.length; i++){
+                            totalPowerConsumption += numberOfAppliances[i]*data[appliances[i]].output;
+                        }
+                        for(let i = numberOfAppliances.length; i<appliances.length; i++){
+                            totalPowerConsumption += data[appliances[i]].output;
+                        }
                     }
-                 }
-            
-            
-            conv.ask("Total Power Consumption is " + totalPowerConsumption);
+            conv.ask("Total Power Consumption is " + totalPowerConsumption + "Watts");
         })
 })
 
